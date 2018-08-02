ae8b22f8e0e9d9b32c767d1331ae88ab7e5a75fd
David Lowe <dlowe30971@live.com.au>
2018-08-02 13:27:05 +1000

    Update rsec-calculate.c

--- src/rsec-calculate.c
+++ src/rsec-calculate.c
@@ -46,9 +46,40 @@ Here are some of the basic rules,
 
 //Create/get consumuables characteristics
 //these numbers will be based on NSWGR D58 class loco
-unit16_t waterTender = 41000;           //represents liters
-unit16_t coalTender = 14000;            //represents kg
+//Loco capacitites
+unit16_t waterCapTender = 41000;           //represented in liters
+unit16_t waterCapBoiler = 1000;            //represented in liters
+unit16_t steamCapBoiler = 5000;            //representd in units of steam
+unit16_t coalCapTender = 14000;            //represented in kg
+unit16_t coalCapFirebox = 1000;            //represented in kg
+unit8_t ashpanCap = 250;                   //represent the Ashpan capacity in kg
+unit8_t sandCapSandbox = 250;              //represented in kg
+unit8_t airCapReservior = 250;             //represented in liters
 
+//Various rates
+unit8_t waterFillRateTender = 10;            //represents the water fill rate from a water tower into the tender per second. At 10 realtime is 68mins for 41,000l fill.
+unit8_t waterFillRateBoiler = 10;            //represents the water transfer rate from tender to boiler via the injectors, liters pre sec.
+unit8_t sandFillRateSandbox = 5;             //represents the sand fill rate to load up the sandbox, kg per sec.
+unit8_t sandDropRateSandbox = 1;             //represents the sand drop rate to the tracks when the sand is activated, kg per sec.
+unit8_t airFillRateReservior = 2;            //represnts the air compressor fill rate to the air reservior, 2 per sec.
+unit8_t airDropRateReservior = 10;           //represents the air used from the reservior per brake application. May have to rethink this with variable braking active?
+unit8_t steamDropRateAirCompressor = 2;      //represents the steam used by the Air Compressor when activated. staem units per sec.
+unit8_t steamDropRateGenerator = 2;          //represents the steam used by the Electricity Generator when activated. staem units per sec.
+unit8_t steamDropRateBlower = 10;            //represents the steam used by the Blower when activated. staem units per sec.
+unit8_t steamDropRateDrianCocks = 5;         //represents the steam used by the Cyclinder Drin Cock are open. staem units per sec.
+unit8_t steamDropRateRelief = 10;            //represents the steam used by the Steam Relief Valve when activated. staem units per sec.
+//train weight multipler use tonnage from cst
+
+//Various levels
+unit16_t waterLevelTender;           //represented in liters
+unit16_t waterLevelBoiler;           //represented in liters
+unit16_t steamLevelBoiler;           //representd in units of steam
+unit16_t coalLevelTender;            //represented in kg
+unit16_t coalLevelFirebox;           //represented in kg
+unit16_t tempreatureFirebox;         //represented in degrees
+unit8_t ashpanLevel;                 //represent the Ashpan capacity in kg
+unit8_t sandLevelSandbox;            //represented in kg
+unit8_t airLevelReservior;           //represented in liters
 
 //Get/update control inputs and status's
 
